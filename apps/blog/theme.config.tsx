a0913559264422a9eb2b1d4418f17972e6e27369
Devon Wells <devon.wellsa@gmail.com>
2022-10-15 13:49:35 -0400

    feat: blog skeleton

---
+++ apps/blog/theme.config.tsx
@@ -0,0 +1,13 @@
+export default {
+  footer: <p>ISC 2022 © Devon Wells</p>,
+  head: ({ meta }: { meta: any }) => (
+    <>
+      {meta.description && (
+        <meta name="description" content={meta.description} />
+      )}
+      {meta.tag && <meta name="keywords" content={meta.tag} />}
+      {meta.author && <meta name="author" content={meta.author} />}
+    </>
+  ),
+  readMore: "Read More →",
+};
