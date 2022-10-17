5e2bb1e67df8e13cc90748f2c6b3d6de980afbee
Devon Wells <devon.wellsa@gmail.com>
2022-10-17 11:50:48 -0400

    feat: favicon in blog

--- apps/blog/theme.config.tsx
+++ apps/blog/theme.config.tsx
@@ -20,6 +20,27 @@ export default {
   ),
   head: ({ meta }: { meta: any }) => (
     <>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <meta name="apple-mobile-web-app-title" content="Wells Blog" />
+      <meta name="application-name" content="Wells Blog" />
+      <meta name="theme-color" content="#ffffff" />
       {meta.description && (
         <meta name="description" content={meta.description} />
       )}
