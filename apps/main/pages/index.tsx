b2ba29c34af08a0543eb6958f56622b25128408a
Devon Wells <devon.wellsa@gmail.com>
2022-10-18 22:22:56 -0400

    feat: migrate files to src and introduce vanilla extract

--- apps/main/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Main() {
-  return (
-    <div>
-      <h1>Dev Wells</h1>      
-    </div>
-  );
-}
---
+++ apps/main/src/styles/app.css.ts
@@ -0,0 +1,6 @@
+import { style } from "@vanilla-extract/css";
+
+// WIP
+export const container = style({
+  padding: 10,
+});
