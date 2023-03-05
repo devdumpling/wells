4b253da7872d3b989d92102de7ef6cb731472657
Devon Wells <devon.wells@pm.me>
2023-03-04 22:36:03 -0500

    Tailwind test (#52)

---
+++ apps/main/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import "../styles/globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: "Wells",
+  description: "Wellscome to my website.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={inter.className}>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
