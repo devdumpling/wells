dcef349835104594e209f311ea3f9ebc9fb10131
Devon Wells <devon.wellsa@gmail.com>
2023-03-05 13:40:00 -0500

    feat: some light styling

--- apps/main/src/app/page.tsx
+++ apps/main/src/app/page.tsx
@@ -2,12 +2,20 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen dark:bg-gradient-to-br dark:from-zinc-700 dark:to-transparent flex">
-      <section className="flex">
-        <h1 className="text-3xl">Devon Wells</h1>
-        <Link href="/blog">blog.</Link>
-        <Link href="https://github.com/devdumpling">code.</Link>
-        <Link href="/docs">docs.</Link>
+    <div className="min-h-screen dark:bg-gradient-to-br dark:from-zinc-700 dark:to-transparent flex items-center justify-center">
+      <section className="py-8 px-16 m-8 shadow-2xl shadow-cyan-800/40 dark:bg-gradient-to-br dark:from-violet-800 dark:to-cyan-800 rounded-3xl">
+        <h1 className="text-7xl font-bold">Dev Wells</h1>
+        <nav>
+          <Link className="mr-2" href="/blog">
+            blog.
+          </Link>
+          <Link className="mr-2" href="https://github.com/devdumpling">
+            code.
+          </Link>
+          <Link className="mr-2" href="/docs">
+            docs.
+          </Link>
+        </nav>
       </section>
     </div>
   );
