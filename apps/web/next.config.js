d005ab7e564297aad020c329f845fba576268ad8
Devon Wells <devon.wellsa@gmail.com>
2023-03-19 09:12:21 -0400

    feat: reconfigure turbo

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,22 @@
+const { DOCS_URL } = process.env;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  async rewrites() {
+    return [
+      {
+        source: "/docs",
+        destination: `${DOCS_URL}/docs`,
+      },
+      {
+        source: "/docs/:path*",
+        destination: `${DOCS_URL}/docs/:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
