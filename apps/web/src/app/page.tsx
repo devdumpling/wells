0f8f7bf2690520fbac63c3afb87b4b41e0f8b3e0
Devon Wells <devon.wellsa@gmail.com>
2023-03-23 09:09:58 -0400

    padding

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -10,9 +10,9 @@ export default function HomePage() {
   });
 
   return (
-    <div className="min-h-screen dark:bg-gradient-to-br dark:from-zinc-700 dark:to-transparent flex flex-col items-center justify-center">
+    <div className="min-h-screen dark:bg-gradient-to-br dark:from-zinc-700 dark:to-transparent flex flex-col items-center justify-center px-4">
       <section className="py-8 px-16 m-8 shadow-2xl shadow-cyan-800/40 dark:bg-gradient-to-br dark:from-violet-800 dark:to-cyan-800 rounded-3xl">
-        <h1 className="text-7xl font-bold">Dev Wells</h1>        
+        <h1 className="text-7xl font-bold">Dev Wells</h1>
         <nav>
           <Link className="mr-2" href="/blog">
             blog.
@@ -26,10 +26,10 @@ export default function HomePage() {
         </nav>
       </section>
       <div className="transition-all duration-300 ease-in-out hover:text-violet-200">
-          <h2 className="text-3xl font-bold my-4">
-            Something of a {copy} myself.
-          </h2>
-        </div>
+        <h2 className="text-3xl font-bold my-4">
+          Something of a {copy} myself.
+        </h2>
+      </div>
     </div>
   );
 }
