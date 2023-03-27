e73ffc4e56ef75323c659c4a6cbca0f88981403a
Devon Wells <devon.wellsa@gmail.com>
2023-03-27 08:03:12 -0400

    fix: try full renaming

--- apps/web/src/content/index.ts
+++ apps/web/src/content/index.ts
@@ -1,8 +1,8 @@
 import * as momentum from "./momentum.mdx";
 import * as bartendersPerspective from "./bartenders-perspective.mdx";
-import * as aLetterToMyPastVeganSelf from "./a-letter-to-my-past-vegan-self.mdx";
+import * as aLetterToMyPastVeganSelf from "./letter-to-vegan-self.mdx";
 import * as fireEmblemPacingProblem from "./Fire-Emblems-Pacing-Problem.mdx";
-import * as howTheNeverGiveUpNarrativeFailedMe from "./how-the-never-give-up-narrative-failed-me.mdx";
+import * as howTheNeverGiveUpNarrativeFailedMe from "./never-give-up-narrative.mdx";
 import * as nostalgiaReviewFinalFantasyX from "./Nostalgia-Review-Final-Fantasy-X.mdx";
 import * as rememberingMaplestory from "./Remembering-MapleStory.mdx";
 import * as stopTryingToMonetizeEverything from "./Stop-Trying-to-Monetize-Everything.mdx";
