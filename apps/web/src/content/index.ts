b6ded5ffe2479a744ee8748da64667214e24fe03
Devon Wells <devon.wellsa@gmail.com>
2023-03-27 07:58:31 -0400

    fix: more casings

--- apps/web/src/content/index.ts
+++ apps/web/src/content/index.ts
@@ -1,8 +1,8 @@
 import * as momentum from "./momentum.mdx";
 import * as bartendersPerspective from "./bartenders-perspective.mdx";
-import * as aLetterToMyPastVeganSelf from "./A-Letter-to-My-Past-Vegan-Self.mdx";
+import * as aLetterToMyPastVeganSelf from "./a-letter-to-my-past-vegan-self.mdx";
 import * as fireEmblemPacingProblem from "./Fire-Emblems-Pacing-Problem.mdx";
-import * as howTheNeverGiveUpNarrativeFailedMe from "./How-the-Never-Give-Up-Narrative-Failed-Me.mdx";
+import * as howTheNeverGiveUpNarrativeFailedMe from "./how-the-never-give-up-narrative-failed-me.mdx";
 import * as nostalgiaReviewFinalFantasyX from "./Nostalgia-Review-Final-Fantasy-X.mdx";
 import * as rememberingMaplestory from "./Remembering-MapleStory.mdx";
 import * as stopTryingToMonetizeEverything from "./Stop-Trying-to-Monetize-Everything.mdx";
