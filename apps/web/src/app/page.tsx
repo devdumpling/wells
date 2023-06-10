2076f29e5345ad17511c24c464e08f2974b004c2
Devon Wells <devon.wells@pm.me>
2023-06-10 09:05:32 -0400

    init: cut down base page and add a css module

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,21 +2,8 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen dark:bg-gradient-to-br dark:from-zinc-700 dark:to-transparent flex flex-col items-center justify-center px-4">
-      <section className="py-8 px-16 m-8 shadow-2xl shadow-cyan-800/40 dark:bg-gradient-to-br dark:from-violet-800 dark:to-cyan-800 rounded-3xl">
-        <h1 className="text-7xl font-bold">Dev Wells</h1>
-        <nav>
-          <Link className="mr-2" href="/blog">
-            blog.
-          </Link>
-          <Link className="mr-2" href="https://github.com/devdumpling">
-            code.
-          </Link>
-          <Link className="mr-2" href="/docs">
-            docs.
-          </Link>
-        </nav>
-      </section>
+    <div>
+      <h1>Devon Wells</h1>
     </div>
   );
 }
