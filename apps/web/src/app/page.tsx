ca2074fa261b957c0da6146ab349dd4127a4081a
Devon Wells <devon.wells@pm.me>
2023-07-01 09:09:52 -0400

    fix: temp

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,6 +4,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Devon Wells</h1>
+      <p>Currently rebuilding my portfolio for the 123094832th time testing some new css features, maybe moving domains, idk. Come back later?</p>
     </div>
   );
 }
