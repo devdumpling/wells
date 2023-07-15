6b36589dc6fffbf8ab1f43502f50abb6bf569c14
Devon Wells <devon.wells@pm.me>
2023-07-15 13:54:52 -0400

    chore: bump deps

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -19,7 +19,14 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body className="bg-white dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100">
-        <main>{children}</main>
+        <div className="fixed inset-0 flex justify-center sm:px-8">
+          <div className="flex w-full max-w-7xl lg:px-8">
+            <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+          </div>
+        </div>
+        <div className="relative">
+          <main>{children}</main>
+        </div>
       </body>
     </html>
   );
