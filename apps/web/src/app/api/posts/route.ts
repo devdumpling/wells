ae183c54812098e87043ef996702ca20c58b2696
Devon Wells <devon.wells@pm.me>
2023-08-05 09:02:17 -0400

    working comment

--- apps/web/src/app/api/posts/route.ts
+++ apps/web/src/app/api/posts/route.ts
@@ -11,5 +11,23 @@ export async function GET() {
 
   console.log(data);
 
+  // data comes back as an array of objects that look like this
+  //   {
+  //      name: 'momentum.md',
+  //      path: 'blog/content/momentum.md',
+  //      sha: '470f3b646205d5deb1e5928b66264c8d312a7d14',
+  //      size: 9704,
+  //      url: 'https://api.github.com/repos/devdumpling/writing/contents/blog/content/momentum.md?ref=main',
+  //      html_url: 'https://github.com/devdumpling/writing/blob/main/blog/content/momentum.md',
+  //      git_url: 'https://api.github.com/repos/devdumpling/writing/git/blobs/470f3b646205d5deb1e5928b66264c8d312a7d14',
+  //      download_url: 'https://raw.githubusercontent.com/devdumpling/writing/main/blog/content/momentum.md',
+  //      type: 'file',
+  //      _links: {
+  //        self: 'https://api.github.com/repos/devdumpling/writing/contents/blog/content/momentum.md?ref=main',
+  //        git: 'https://api.github.com/repos/devdumpling/writing/git/blobs/470f3b646205d5deb1e5928b66264c8d312a7d14',
+  //        html: 'https://github.com/devdumpling/writing/blob/main/blog/content/momentum.md'
+  //      }
+  //  }
+
   return NextResponse.json(data);
 }
