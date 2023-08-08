59b9d4c60d28a78fa30e43959831a4d66066a721
Devon Wells <devon.wells@pm.me>
2023-08-08 11:39:19 -0400

    feat(posts)

--- apps/web/src/app/api/posts/route.ts
+++ apps/web/src/app/api/posts/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { Octokit } from "octokit";
+import matter from "gray-matter";
 
 export async function GET() {
   const octokit = new Octokit();
@@ -9,25 +10,21 @@ export async function GET() {
     path: "blog/content",
   });
 
-  console.log(data);
+  const posts = await Promise.all(
+    data.map(async (post: any) => {
+      const { data: content } = await octokit.request(post.url);
+      const { data: file } = await octokit.request(content.download_url);
+      const { data: frontmatter, content: markdownContent } = matter(file);
 
-  // data comes back as an array of objects that look like this
-  //   {
-  //      name: 'momentum.md',
-  //      path: 'blog/content/momentum.md',
-  //      sha: '470f3b646205d5deb1e5928b66264c8d312a7d14',
-  //      size: 9704,
-  //      url: 'https://api.github.com/repos/devdumpling/writing/contents/blog/content/momentum.md?ref=main',
-  //      html_url: 'https://github.com/devdumpling/writing/blob/main/blog/content/momentum.md',
-  //      git_url: 'https://api.github.com/repos/devdumpling/writing/git/blobs/470f3b646205d5deb1e5928b66264c8d312a7d14',
-  //      download_url: 'https://raw.githubusercontent.com/devdumpling/writing/main/blog/content/momentum.md',
-  //      type: 'file',
-  //      _links: {
-  //        self: 'https://api.github.com/repos/devdumpling/writing/contents/blog/content/momentum.md?ref=main',
-  //        git: 'https://api.github.com/repos/devdumpling/writing/git/blobs/470f3b646205d5deb1e5928b66264c8d312a7d14',
-  //        html: 'https://github.com/devdumpling/writing/blob/main/blog/content/momentum.md'
-  //      }
-  //  }
+      console.log(frontmatter);
+      return {
+        frontmatter,
+        markdownContent,
+      };
+    })
+  );
 
-  return NextResponse.json(data);
+  console.log(posts);
+
+  return NextResponse.json(posts);
 }
