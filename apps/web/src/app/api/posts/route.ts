9925db9e0ba5622e268610587aa3c6a89fa83c6d
Devon Wells <devon.wells@pm.me>
2023-08-09 08:11:20 -0500

    type api/posts

--- apps/web/src/app/api/posts/route.ts
+++ apps/web/src/app/api/posts/route.ts
@@ -2,19 +2,21 @@ import { NextResponse } from "next/server";
 import { Octokit } from "octokit";
 import matter from "gray-matter";
 
-export async function GET() {
-  const octokit = new Octokit();
-  const { data } = await octokit.rest.repos.getContent({
+import type { Post } from "./types";
+
+export async function GET(): Promise<NextResponse> {
+  const octokit: Octokit = new Octokit();
+  const { data }: any = await octokit.rest.repos.getContent({
     owner: "devdumpling",
     repo: "writing",
     path: "blog/content",
   });
 
-  const posts = await Promise.all(
+  const posts: Post[] = await Promise.all(
     data.map(async (post: any) => {
-      const { data: content } = await octokit.request(post.url);
-      const { data: file } = await octokit.request(content.download_url);
-      const { data: frontmatter, content: markdownContent } = matter(file);
+      const { data: content }: any = await octokit.request(post.url);
+      const { data: file }: any = await octokit.request(content.download_url);
+      const { data: frontmatter, content: markdownContent }: any = matter(file);
 
       console.log(frontmatter);
       return {
