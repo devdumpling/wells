6831831fdec68a1a2f7adf466be05851692f06fc
Devon Wells <devon.wells@pm.me>
2023-08-09 21:41:09 -0500

    fix: add components to MDXRemote

--- apps/web/src/app/blog/[slug]/page.tsx
+++ apps/web/src/app/blog/[slug]/page.tsx
@@ -5,10 +5,17 @@ export default async function Page({ params }: { params: { slug: string } }) {
   const post = await getPost(params.slug);
   const { frontmatter, markdownContent } = post;
 
+  const components = {
+    h1: (props: any) => <h1 className="my-4 text-4xl font-bold" {...props} />,
+    h2: (props: any) => <h2 className="my-4 text-2xl font-bold" {...props} />,
+    h3: (props: any) => <h3 className="my-4 text-xl font-bold" {...props} />,
+    p: (props: any) => <p className="my-4" {...props} />,
+  };
+
   return (
     <section>
       <h1 className="my-8 text-4xl font-bold">{frontmatter.title}</h1>
-      <MDXRemote source={markdownContent} />
+      <MDXRemote source={markdownContent} components={components} />
     </section>
   );
 }
