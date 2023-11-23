6c5c7693eb465be445024554c5e24d982373af20
Devon Wells <devon.wells@pm.me>
2023-11-23 08:45:46 -0500

    header

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,9 @@
+import Header from "@/components/Header";
+
 export default async function HomePage() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
---
+++ apps/web/src/components/Header.tsx
@@ -0,0 +1,12 @@
+const Header: React.FC = () => {
+  return (
+    <header className="container flex items-center justify-between py-4 mx-auto">
+      <div className="text-foreground">
+        <h1 className="text-3xl">Devon Wells</h1>
+        <p className="text-xl font-light">LV. 30</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
