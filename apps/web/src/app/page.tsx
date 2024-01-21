2642ea09f3ce73e68eb17ddb6053b596ab604d78
Devon Wells <devon.wells@pm.me>
2024-01-21 09:25:15 -0500

    fix: tidy up and move copy around

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,17 +7,9 @@ export default async function Character() {
   return (
     <section className="flex flex-col items-center justify-center">
       <div className="flex flex-col justify-around max-w-screen-xl m-2 tracking-wide md:flex-row rounded-3xl backdrop-blur-sm font-primary text-mono">
-        <div className="flex flex-col gap-8 p-8 basis-1/2 rounded-3xl">
-          <div className="flex flex-col">
-            <div className="flex flex-row items-baseline gap-2 pb-4 border-b-4">
-              <h1 className="font-mono text-2xl font-bold">dev.wells</h1>
-              <h2 className="text-xl italic text-muted-foreground">
-                lvl 30 | they/he
-              </h2>
-            </div>
-          </div>
-          <StatBlock />
+        <div className="flex flex-col gap-8 p-8 basis-1/2">
           <CharacterInfo />
+          <StatBlock />
         </div>
 
         <div className="relative px-4 py-8 h-1/3 sm:h-1/2 sm:w-1/2">
--- apps/web/src/components/CharacterInfo.tsx
+++ apps/web/src/components/CharacterInfo.tsx
@@ -1,8 +1,14 @@
 const CharacterInfo = () => {
   return (
-    <div className="space-y-4">
-      <h2 className="text-2xl font-bold font-pixelify">characteristics</h2>
-      <p className="font-serif italic">
+    <div className="flex flex-col">
+      <div className="flex flex-row items-baseline gap-2 pb-4 border-b-4">
+        <h1 className="font-mono text-2xl font-bold">dev.wells</h1>
+        <h2 className="text-xl italic text-muted-foreground">
+          lvl 30 | they/he
+        </h2>
+      </div>
+
+      <p className="my-4 font-serif italic">
         I dwell in the midwest, raising my daughter, perfecting vegan mac,
         tinkering, playing games, and pretending to enjoy the cold.
       </p>
